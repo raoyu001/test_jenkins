6d20fe996e03eb277933858c3f19f0ceaf9f2bcd
raoyu000 <java_test012@163.com>
2019-03-06 15:06:53 +0800

    区分环境（未实现）

--- test_jenkins_web/src/main/java/com/test/jenkins/TestJenkinsApplication.java
+++ test_jenkins_web/src/main/java/com/test/jenkins/TestJenkinsApplication.java
@@ -3,12 +3,22 @@ package com.test.jenkins;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StringUtils;
 
 @MapperScan("com.test.jenkins.dao")
 @SpringBootApplication(scanBasePackages = "com.test.jenkins")
 public class TestJenkinsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TestJenkinsApplication.class,args);
+        SpringApplication app = new SpringApplication(TestJenkinsApplication.class);
+        //setApp(app);
+        app.run(args);
+    }
+
+    private static void setApp(SpringApplication app) {
+        String activeProfile = System.getProperty("spring.profiles.active");
+        if (StringUtils.isEmpty(activeProfile)) {
+            app.setAdditionalProfiles("qa");
+        }
     }
 }
