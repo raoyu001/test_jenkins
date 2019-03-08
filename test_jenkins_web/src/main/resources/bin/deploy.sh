fad9abb4537087255478497419ac906df9a8bd59
raoyu000 <java_test012@163.com>
2019-03-08 11:13:15 +0800

    修改部署脚本

--- test_jenkins_web/src/main/resources/bin/deploy.sh
+++ test_jenkins_web/src/main/resources/bin/deploy.sh
@@ -1,42 +1,6 @@
-#!/usr/bin/env bash
-#编译+部署order站点
-
-#需要配置如下参数
-# 项目路径, 在Execute Shell中配置项目路径, pwd 就可以获得该项目路径
-# export PROJ_PATH=这个jenkins任务在部署机器上的路径
-
-# 输入你的环境上tomcat的全路径
-# export TOMCAT_APP_PATH=tomcat在部署机器上的路径
-
-### base 函数
-killTomcat()
-{
-    pid=`ps -ef|grep tomcat|grep java|awk '{print $2}'`
-    echo "tomcat Id list :$pid"
-    if [ "$pid" = "" ]
-    then
-      echo "no tomcat pid alive"
-    else
-      kill -9 $pid
-    fi
-}
-cd $PROJ_PATH/order
-mvn clean install
-
-# 停tomcat
-killTomcat
-
-# 删除原有工程
-rm -rf $TOMCAT_APP_PATH/webapps/ROOT
-rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
-rm -f $TOMCAT_APP_PATH/webapps/test_jenkins.war
-
-# 复制新的工程
-cp $PROJ_PATH/order/target/test_jenkins.war $TOMCAT_APP_PATH/webapps/
-
-cd $TOMCAT_APP_PATH/webapps/
-mv test_jenkins.war ROOT.war
-
-# 启动Tomcat
-cd $TOMCAT_APP_PATH/
-sh bin/startup.sh
+#!/bin/bash
+PID=$(netstat -tnpl | grep :7200 | awk '{print $7}' | awk -F '/' '{print $1}')
+kill -9 $PID
+echo "kill process at pid = [$PID]"
+nohup java -jar /test_dir/test_jenkins/test_jenkins.jar &
+echo 'spring boot reload success'
